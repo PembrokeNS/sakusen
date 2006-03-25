1772f00042227fe7fd6f2a77b85b82192e9a793b
simeon <simeon@6f43db7c-3df8-0310-95f7-eae406835aab>
2006-03-25 21:14:47 +0000

    John is a harsh taskmaster, so I did some very preliminary stuff on porting libsakusen-resources.
    git-svn-id: file:///var/lib/svn/sakusen/cpp/trunk@297 6f43db7c-3df8-0310-95f7-eae406835aab

--- libsakusen/resources/libsakusen-resources-global.h
+++ libsakusen/resources/libsakusen-resources-global.h
@@ -10,5 +10,22 @@
 /** FILENAME_REGEX defines a regex which must be matched by filenames */
 #define FILENAME_REGEX "[-a-z0-9_.]+"
 
+
+
+
+#if defined(_MSC_VER)
+  /* The __declspec stuff for ensuring symbols are exported from DLLs and
+   * imported back into libraries */
+  #ifdef LIBSAKUSEN_RESOURCES_EXPORTS
+    #define LIBSAK_RES_API   __declspec(dllexport)
+    #define LIBSAK_RES_EXIMP extern
+  #else
+    #define LIBSAK_RES_API   __declspec(dllimport)
+    #define LIBSAK_RES_EXIMP
+  #endif
+#else
+  #define LIBSAKUSEN_RESOURCES_API
+#endif
+
 #endif // LIBSAKUSEN_RESOURCES_GLOBAL_H
 
