9f25eb395828e94b58a90c29020858da75a6b498
z1 <z1@6f43db7c-3df8-0310-95f7-eae406835aab>
2006-09-09 22:41:07 +0000

    * doxygenate quadratic.h a bit * add a few extra methods to Rectangle and work on handling invalid and empty Rectangles a bit better
    git-svn-id: file:///var/lib/svn/sakusen/cpp/trunk@361 6f43db7c-3df8-0310-95f7-eae406835aab

--- libsakusen/quadratic.h
+++ libsakusen/quadratic.h
@@ -6,16 +6,31 @@
 
 namespace sakusen {
 
-/* store a ballistic curve, x and y linear in T, z quadratic in T
+/** \brief A virtual quadratic curve.
+ *
+ * Just as the Ray class represents a ray in game-space that doesn't interact
+ * with objects, this class represents a quadratic in game-space. It is
+ * intended to store the path of a ballistic projectile, so it is linear in x
+ * and y, and the z co-ordinate is acted on by gravity.
+ *
+ * The Ballistic class extends this one to represent an actual object in the
+ * game-world that has Quadratic geometry but interacts with other objects.
+ *
+ * The equation for the quadratic is:
+ *
  * x(T) = sx + dx.(T-t)
  * y(T) = sy + dy.(T-t)
  * z(T) = sz + dz.(T-t) - g.(T-t)^2
+ *
+ * where \c s is the origin (or start point), \c d is the initial velocity,
+ * \c t is the time of firing (the time at which the curve passes through the
+ * origin), and \em g is the acceleration due to gravity (depends on map).
  */
 class Quadratic {
   private:
-  Point<sint32> s;
-  Point<sint32> d;
-  Time t;
+  Point<sint32> s; /**< origin */
+  Point<sint32> d; /**< direction */
+  Time t; /**< time origin */
   public:
   Quadratic(): s(0,0,0), d(0,0,0), t(0) {}
   Quadratic(Point<sint32> start, Point<sint32> velocity, Time startTime): s(start), d(velocity), t(startTime) {}
--- libsakusen/rectangle.h
+++ libsakusen/rectangle.h
@@ -8,8 +8,8 @@
 /** \file
  * A ::Rectangle is the region enclosed by two points in the plane. The
  * bottom-left point is inside the rectangle but the top-right point is
- * outside. This means you can't have a rectangle which all points are inside
- * but you can have a rectangle with zero area.
+ * outside. This means you can't have a rectangle which all points are
+ * inside but you can have a rectangle with zero area.
  *
  * \todo What happens when you have a Rectangle<sint32> whose points are
  * greater than 2^31 apart and you take the width/height?
@@ -27,10 +27,12 @@ struct LIBSAKUSEN_API Rectangle {
   T maxx; /**< The maximum value of x */
   T maxy; /**< The maximum value of y */
 
-  /* methods */
+  /** \brief Construct an empty Rectangle. */
   Rectangle() : minx(0), miny(0), maxx(0), maxy(0) {}
-  /** Constructor, does the obvious thing. It must be the case that \p x1 \< \p
-   * x2 and \p y1 \< \p y2.
+  /** \brief Construct a Rectangle with the given corners.
+   *
+   * It must be the case that \p x1 \< \p x2 and \p y1 \< \p y2. This is not
+   * checked.
    *
    * \param x1,y1 Bottom-left corner
    * \param x2,y2 Top-right corner
@@ -47,6 +49,7 @@ struct LIBSAKUSEN_API Rectangle {
   inline T getMinY() const { return miny; }
   inline T getMaxX() const { return maxx; }
   inline T getMaxY() const { return maxy; }
+  inline bool isEmpty() const { return (getMinX() >= getMaxX() && getMinY() >= getMaxY()); }
   inline bool contains(const Point<T>& p) const {
     return p.x >= minx && p.x < getMaxX() && p.y >= miny && p.y < getMaxY();
   }
@@ -56,7 +59,13 @@ struct LIBSAKUSEN_API Rectangle {
   }
   bool fastIntersects(const ICompleteUnit*) const;
   bool fastIntersects(const ISensorReturns*) const;
+  /** \brief Computes the intersection of intersecting Rectangles.
+   *
+   * If the rectangles have a non-empty intersection, returns the Rectangle of
+   * their intersection. If they do not intersect, returns an empty Rectangle.
+   */
   inline Rectangle<T> intersect(const Rectangle<T>& right) const {
+    if (!intersects(right)) return Rectangle();
     return Rectangle<T>(
         static_cast<T>(std::max(minx, right.minx)),
         static_cast<T>(std::max(miny, right.miny)),
@@ -64,7 +73,9 @@ struct LIBSAKUSEN_API Rectangle {
         static_cast<T>(std::min(maxy, right.maxy))
       );
   }
+  Rectangle<T> enclosure(const Rectangle<T>& right) const;
 
+  bool operator==(const Rectangle<T>& right) const;
   void store(OArchive& out) const {
     out << minx << miny << maxx << maxy;
   }
@@ -78,6 +89,42 @@ struct LIBSAKUSEN_API Rectangle {
   }
 };
 
+#ifdef LIBSAKUSEN_METHOD_DEFINITIONS
+/** \brief Get the smallest Rectangle enclosing both.
+ *
+ * Returns the smallest rectangle that completely contains both this
+ * Rectangle and the given Rectangle. If either is empty, returns a copy of
+ * the other; if both are empty, returns an empty Rectangle.
+ */
+template<typename T>
+Rectangle<T> Rectangle<T>::enclosure(const Rectangle<T>& right) const {
+  if (isEmpty()) {
+    if (right.isEmpty()) return Rectangle();
+    else return right;
+  } else if (right.isEmpty()) {
+    return *this;
+  }
+  return Rectangle<T>(
+      static_cast<T>(std::min(minx, right.minx)),
+      static_cast<T>(std::min(miny, right.miny)),
+      static_cast<T>(std::max(maxx, right.maxx)),
+      static_cast<T>(std::max(maxy, right.maxy))
+    );
+}
+
+/** \brief Compare two Rectangles for equality.
+ *
+ * Returns true if either or both of the following conditions is met:
+ * all four points of one Rectangle are equal to the corresponding points of the other; or
+ * both rectangles are empty
+ */
+template<typename T>
+bool Rectangle<T>::operator==(const Rectangle<T>& right) const {
+  return ((minx == right.minx && miny == right.miny
+      && maxx == right.maxx && maxy == right.maxy)
+      || (isEmpty() && right.isEmpty()));
+}
+#endif
 }
 
 #endif // UI__RECTANGLE_H
