1a19e6f898ccf65741f32aadd7a41e254a66e22d
john <john@6f43db7c-3df8-0310-95f7-eae406835aab>
2007-06-04 20:36:02 +0000

    r346@sammy:  john | 2007-06-03 16:23:17 +0100  - Ensured solveQuadratic can cope properly with more cases, and document    exactly what it's supposed to do.
    git-svn-id: file:///var/lib/svn/sakusen/cpp/trunk@449 6f43db7c-3df8-0310-95f7-eae406835aab

--- libsakusen/mathsutils.h
+++ libsakusen/mathsutils.h
@@ -8,25 +8,41 @@
 
 namespace sakusen {
 
+/** \brief Solves a quadratic and returns all available roots
+ *
+ * The parameters are \p a, \p b and \p c, where the quadratic to be solved is
+ * ax^2+bx+c = 0, and we are solving for x.
+ *
+ * If a=b=0 then returns (NaN,NaN).
+ * If a=0 and b!=0 then returns (x, NaN) where x is the root of the linear
+ * equation.
+ * If a!=0 and there is no root returns (NaN,Nan).
+ * If a!=0 and there is a repeated root x returns (x,x).
+ * If a!=0 and there are distinct roots x and y then returns (x,y) where x is
+ * guaranteed to be less than y.
+ */
 inline boost::tuple<double,double> mathsUtils_solveQuadratic(
     const double a,
     const double b,
     const double c
   )
 {
+  /* I don't actually know what the differrence between a quiet and a signaling
+   * NaN are, possibly this is the wrong sort */
+  assert(std::numeric_limits<double>::has_quiet_NaN);
+  const double nan = std::numeric_limits<double>::quiet_NaN();
   /* cope with the linear case */
   if (a==0) {
-    return -c/b;
+    if (b==0) {
+      return boost::make_tuple(nan, nan);
+    }
+    return boost::make_tuple(-c/b, nan);
   }
 
   /* discriminant */
   const double d = b*b - 4*a*c;
 
-  /* I don't actually know what the differrence between a quiet and a signaling
-   * NaN are, possibly this is the wrong sort */
-  assert(std::numeric_limits<double>::has_quiet_NaN);
   if (d < 0) {
-    const double nan = std::numeric_limits<double>::quiet_NaN();
     return boost::make_tuple(nan, nan);
   }
 
