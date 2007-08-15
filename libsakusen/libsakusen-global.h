PembrokeNS/sakusen

commit df447ebf589056422ba645a1baee005bb7c88b3c

Tedomari now compiles on windows.
git-svn-id: file:///var/lib/svn/sakusen/cpp/trunk@507 6f43db7c-3df8-0310-95f7-eae406835aab

libsakusen/libsakusen-global.h
@@ -39,7 +39,8 @@
    * was very important.  In the long run we should do without disabling 
    * this warning. */
 //  #pragma warning(disable: 4251)
-  #pragma warning(disable: 4715)
+  #pragma warning(disable: 4715 4345)
+   /* 4345 is a warning about a change from VS 2003. We don't care. 
 
   /* 4715: Disable "not all code paths return a value" warning because the
    * compiler is too stupid to exhibit it properly */

libsakusen/resources/fileresourceinterface.cpp
@@ -311,7 +311,7 @@ void* FileResourceInterface::internalSymbolSearch(
   lt_dlhandle moduleHandle = lt_dlopenext(modulePath.c_str());
   if (moduleHandle == NULL) {
     *result = resourceSearchResult_error;
-    error = String("lt_dlopen() failed: ") + lt_dlerror();
+    error = "lt_dlopen() failed: " + lt_dlerror();
     return NULL;
   }
   /* We could make the module resident (it cannot be closed) so as to avoid
@@ -395,7 +395,7 @@ bool FileResourceInterface::internalSave(
     }
 
     if (!pcrecpp::RE(FILENAME_REGEX).FullMatch(shortName)) {
-      error = String("Name '") + shortName + "' contains invalid characters and "
+      error = "Name '" + shortName + "' contains invalid characters and "
         "cannot be used as part of a filename.  The name must match " +
         FILENAME_REGEX + ".";
       return true;

fuseki/ide/msvc8/fuseki.vcproj
@@ -124,7 +124,7 @@
 				EnableIntrinsicFunctions="true"
 				EnableFiberSafeOptimizations="true"
 				AdditionalIncludeDirectories="..\..\..\externals\STLport\stlport;..\..\..\externals\pcrecpp;..\..\..\liboptimal;..\..\..\libsakusen;..\..\..\libsakusen\comms;..\..\..\libsakusen\resources;..\..\..\libsakusen\server;..\..\"
-				PreprocessorDefinitions="WIN32;_DEBUG;_CONSOLE;DISABLE_UNIX_SOCKETS"
+				PreprocessorDefinitions="WIN32;NDEBUG;_CONSOLE;DISABLE_UNIX_SOCKETS;DISABLE_AVAHI"
 				MinimalRebuild="true"
 				RuntimeLibrary="2"
 				UsePrecompiledHeader="0"

libsakusen/client/ide/msvc8/libsakusen-client.vcproj
@@ -53,7 +53,7 @@
 				EnableFiberSafeOptimizations="true"
 				WholeProgramOptimization="true"
 				AdditionalIncludeDirectories="..\..\..\..\externals\STLport\stlport;..\..\;..\..\..\"
-				PreprocessorDefinitions="_DEBUG;_WINDOWS;_USRDLL;LIBSAKUSEN_CLIENT_EXPORTS;WIN32"
+				PreprocessorDefinitions="NDEBUG;_WINDOWS;_USRDLL;LIBSAKUSEN_CLIENT_EXPORTS;WIN32"
 				StringPooling="true"
 				RuntimeLibrary="2"
 				EnableFunctionLevelLinking="true"

libsakusen/comms/ide/msvc8/libsakusen-comms.vcproj
@@ -52,8 +52,8 @@
 				EnableIntrinsicFunctions="true"
 				EnableFiberSafeOptimizations="true"
 				WholeProgramOptimization="true"
-				AdditionalIncludeDirectories="../../;..\..\..\;..\..\..\..\STLport\stlport"
-				PreprocessorDefinitions="WIN32;_DEBUG;_WINDOWS;_USRDLL;LIBSAKUSEN_COMMS_EXPORTS;DISABLE_UNIX_SOCKETS"
+				AdditionalIncludeDirectories="../../;..\..\..\;..\..\..\..\externals\STLport\stlport"
+				PreprocessorDefinitions="WIN32;NDEBUG;_WINDOWS;LIBSAKUSEN_COMMS_EXPORTS;DISABLE_UNIX_SOCKETS"
 				StringPooling="false"
 				SmallerTypeCheck="false"
 				RuntimeLibrary="2"

libsakusen/ide/msvc8/libsakusen.vcproj
@@ -54,11 +54,10 @@
 				EnableFiberSafeOptimizations="true"
 				WholeProgramOptimization="true"
 				AdditionalIncludeDirectories="..\..\..\externals\STLport\stlport;&quot;..\..\..\externals\mhash-win32\include&quot;;&quot;..\..\..\externals\mhash-win32\lib&quot;"
-				PreprocessorDefinitions="WIN32;NDEBUG;_WINDOWS;_USRDLL;LIBSAKUSEN_EXPORTS"
+				PreprocessorDefinitions="WIN32;NDEBUG;_WINDOWS;LIBSAKUSEN_EXPORTS"
 				StringPooling="true"
 				RuntimeLibrary="2"
 				EnableFunctionLevelLinking="true"
-				RuntimeTypeInfo="true"
 				PrecompiledHeaderFile=".\Release/libsakusen.pch"
 				AssemblerListingLocation=".\Release/"
 				ObjectFile=".\Release/"

libsakusen/resources/ide/msvc8/libsakusen-resources.vcproj
@@ -53,7 +53,7 @@
 				EnableFiberSafeOptimizations="true"
 				WholeProgramOptimization="true"
 				AdditionalIncludeDirectories="..\..\..\..\externals\STLport\stlport;..\..\..\..\externals\pcrecpp;..\..\;..\..\..\;..\..\..\comms"
-				PreprocessorDefinitions="_DEBUG;_WINDOWS;_USRDLL;LIBSAKUSEN_RESOURCES_EXPORTS;WIN32"
+				PreprocessorDefinitions="NDEBUG;_WINDOWS;_USRDLL;LIBSAKUSEN_RESOURCES_EXPORTS;WIN32"
 				StringPooling="true"
 				MinimalRebuild="true"
 				RuntimeLibrary="2"

libsakusen/server/ide/msvc8/libsakusen-server.vcproj
@@ -53,7 +53,7 @@
 				EnableFiberSafeOptimizations="true"
 				WholeProgramOptimization="true"
 				AdditionalIncludeDirectories="..\..\..\..\externals\STLport\stlport;..\..\;..\..\..\"
-				PreprocessorDefinitions="_DEBUG;_WINDOWS;_USRDLL;LIBSAKUSEN_SERVER_EXPORTS;WIN32"
+				PreprocessorDefinitions="NDEBUG;_WINDOWS;_USRDLL;LIBSAKUSEN_SERVER_EXPORTS;WIN32"
 				StringPooling="true"
 				RuntimeLibrary="2"
 				EnableFunctionLevelLinking="false"

commit d455266e3d86d60551862c849b9a9e7a027157d0

Fix build and other breakage introduced by df447ebf589056422ba645a1baee005bb7c88b3c (r507).
In particular, get rid of a /* introduced inside a comment, and revert the
removal of necessary String("literal") constructors, which caused build
breakage under gcc and nasal demons on MSVC.

Signed-off-by: Daniel Hulme <st@istic.org>

libsakusen/libsakusen-global.h
@@ -40,11 +40,10 @@
    * this warning. */
 //  #pragma warning(disable: 4251)
   #pragma warning(disable: 4715 4345)
-   /* 4345 is a warning about a change from VS 2003. We don't care. 
-
-  /* 4715: Disable "not all code paths return a value" warning because the
+  /* 4345 is a warning about a change from VS 2003. We don't care. 
+   * 4715: Disable "not all code paths return a value" warning because the
    * compiler is too stupid to exhibit it properly */
- 
+
 	//Alias of __PRETTY_FUNCTION for 
   #define __PRETTY_FUNCTION__ __FUNCDNAME__
 

libsakusen/resources/fileresourceinterface.cpp
@@ -311,7 +311,7 @@ void* FileResourceInterface::internalSymbolSearch(
   lt_dlhandle moduleHandle = lt_dlopenext(modulePath.c_str());
   if (moduleHandle == NULL) {
     *result = resourceSearchResult_error;
-    error = "lt_dlopen() failed: " + lt_dlerror();
+    error = String("lt_dlopen() failed: ") + lt_dlerror();
     return NULL;
   }
   /* We could make the module resident (it cannot be closed) so as to avoid
@@ -395,7 +395,7 @@ bool FileResourceInterface::internalSave(
     }
 
     if (!pcrecpp::RE(FILENAME_REGEX).FullMatch(shortName)) {
-      error = "Name '" + shortName + "' contains invalid characters and "
+      error = String("Name '") + shortName + "' contains invalid characters and "
         "cannot be used as part of a filename.  The name must match " +
         FILENAME_REGEX + ".";
       return true;