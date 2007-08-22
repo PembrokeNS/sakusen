PembrokeNS/sakusen

commit 28fcc4f5131f0261c2982b9b6ab9535a84bc70b2

Update revision.h generation for git. (Still works with svn.)
Makefile.common now generates REVISION from svn or git or makes it be
"unknown" otherwise. This means that REVISION is now a string rather than
an int, so I've changed all references to it to cope.

Signed-off-by: Daniel Hulme <st@istic.org>


git-svn-id: file:///var/lib/svn/sakusen/cpp/trunk@495 6f43db7c-3df8-0310-95f7-eae406835aab

tedomari/serverinterface.cpp
@@ -64,11 +64,7 @@ void ServerInterface::initialSettingsSetup()
   /** \todo deal with return values */
   setClientSetting("application:name", APPLICATION_NAME);
   setClientSetting("application:version", APPLICATION_VERSION);
-#ifdef REVISION
   setClientSetting("application:revision", numToString(REVISION));
-#else
-  setClientSetting("application:revision", "unknown");
-#endif
 
   /* we request the universe name and hash so that our game can be set up
    * correctly if they were set before we joined */

fuseki/server.cpp
@@ -88,11 +88,7 @@ Server::Server(
   assert(reason == "");
   reason = settings->changeRequest(
       "server:application:revision",
-#if defined(REVISION)
-      numToString(REVISION),
-#else
-      "unknown",
-#endif
+      REVISION,
       this
     );
   assert(reason == "");

makefiles/Makefile.common
@@ -310,23 +310,27 @@ $(MAKEFRAGS): .makefrags/%.makefrag:
 
 # If we're in a working copy then we can make a revision.h containing the
 # current revision number if so desired (this should be automatically
-# generated iff it's included from anywhere).  If we're not in a working copy,
-# then we don't define anything.
+# generated iff it's included from anywhere).
 
-revision.h: $(wildcard $(top_srcdir).svn/entries)
-	if test -f $(top_srcdir).svn/entries ; \
+# Wildcarding these deps makes the file be regenerated after an update but
+# doesn't break make when they don't exist.
+revision.h: $(wildcard $(top_srcdir).svn/entries) $(wildcard $(top_srcdir).git/HEAD)
+	@if test -f $(top_srcdir).svn/entries ; \
 	then \
 		REVISION=`svn info -R $(top_srcdir) | grep "^Revision:" | \
 			awk '{print $$2}' | sort -nr | head -n 1`; \
-		if test "x$$REVISION" != "x"; \
-		then \
-			echo '#define REVISION ' "$$REVISION" > $@; \
-		else \
-		echo '' > $@; \
-		fi; \
+	elif test -d $(top_srcdir).git ; \
+	then \
+		REVISION=`git-rev-list --max-count=1 HEAD`; \
 	else \
-		echo '' > $@; \
-	fi
+		REVISION='unknown' ; \
+	fi; \
+	if test "x$$REVISION" != "x"; \
+	then \
+		echo "#define REVISION \"$$REVISION\"" > $@; \
+	else \
+	echo '' > $@; \
+	fi;
 
 .PHONY: \
 		all all-nonrecursive $(ALL_RECURSIONS) \

tedomari/main.cpp
@@ -183,9 +183,7 @@ int main(int argc, char const* const* argv)
 
   if (options.version) {
     cout << APPLICATION_NAME " " APPLICATION_VERSION;
-#if defined(REVISION)
     cout << " (revision " << REVISION << ")";
-#endif
     cout << endl;
     exit(EXIT_SUCCESS);
   }

commit 66b4a1b1dcff0492f5d735e77bba543362b48745

Looks like I missed a numToString(REVISION) in tedomari.
This fixes a bug introduced by 28fcc4f5131f0261c2982b9b6ab9535a84bc70b2 (r495).
I'm surprised it didn't break the build, actually, but meh.

Signed-off-by: Daniel Hulme <st@istic.org>

tedomari/serverinterface.cpp
@@ -62,7 +62,7 @@ void ServerInterface::initialSettingsSetup()
   /** \todo deal with return values */
   setClientSetting("application:name", APPLICATION_NAME);
   setClientSetting("application:version", APPLICATION_VERSION);
-  setClientSetting("application:revision", numToString(REVISION));
+  setClientSetting("application:revision", REVISION);
 
   /* we request the universe name and hash so that our game can be set up
    * correctly if they were set before we joined */