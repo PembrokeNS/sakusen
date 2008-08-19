49aa15f939bfc287ae66beddf77c07fb6de26149
John Bytheway <jbytheway@gmail.com>
2008-08-19 22:22:06 +0100

    tedomari::Converter error handling tweaks
    - Initialize error codes to U_ZERO_ERROR to prevent spurious error reports.
    - Error reports include the name of the error.
    
    Signed-off-by: John Bytheway <jbytheway@gmail.com>

--- tedomari/converter.cpp
+++ tedomari/converter.cpp
@@ -33,26 +33,26 @@ Converter::~Converter(){}
 /** \brief Converts a string from the native codeset to UTF-8. 
  *   \todo There are more efficient/powerful ways to do this.  */
 String Converter::convertNativeToUTF8(const String& s) {
-  UErrorCode errorCode;
+  UErrorCode errorCode = U_ZERO_ERROR;
   char out[CONVERT_BUFFER_LEN];
   const char* mobileIn = s.c_str();
   size_t outBytesLeft = CONVERT_BUFFER_LEN;
   if (-1 == ucnv_convert("UTF-8", nativeLocale, out, outBytesLeft, mobileIn, 1+s.length(), &errorCode) || U_FAILURE(errorCode))
   {
-    Debug("error converting string");
+    Debug("error converting string: " << u_errorName(errorCode));
     return s;
   }
   return String(out);
 }
 /** \brief Converts a string from UTF-8 to the native codeset */
 String Converter::convertUTF8ToNative(const String& s) {
-  UErrorCode errorCode;
+  UErrorCode errorCode = U_ZERO_ERROR;
   char out[CONVERT_BUFFER_LEN];
   const char* mobileIn = s.c_str();
   size_t outBytesLeft = CONVERT_BUFFER_LEN;
   if (-1 == ucnv_convert(nativeLocale, "UTF-8", out, outBytesLeft, mobileIn, 1+s.length(), &errorCode) || U_FAILURE(errorCode))
   {
-    Debug("error converting string");
+    Debug("error converting string: " << u_errorName(errorCode));
     return s;
   }
   return String(out);
