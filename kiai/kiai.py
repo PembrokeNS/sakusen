9d778e76e313152849538352f64a724ee88cca40
Michael Donaghy <md401@tommy.(none)>
2009-09-14 08:38:59 +0100

    A little more work towards refactoring
    Signed-off-by: Michael Donaghy <md401@tommy.(none)>

--- kiai/kiai.py
+++ kiai/kiai.py
@@ -1,24 +1,25 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from PyQt4 import QtCore,QtGui,QtOpenGL
+from PyQt4 import QtCore, QtGui, QtOpenGL
 from PyKDE4 import kdecore,kdeui
 from connectDialog import Ui_connectDialog
 from settingsDialogImpl import settingsDialog
 from settingsModel import settingsModel
 from gameModel import *
-from listen import listener
 from modeltest import ModelTest
 from mainWindowImpl import mainWindow
 from mapModel import mapModel
 from sceneModel import sceneModel
-import sys,imp
-from sakusen_resources import fileUtils_getHome,path
+import sys, imp, string
+from sakusen_resources import fileUtils_getHome, path
 from sakusen import CONFIG_SUBDIR
 from sakusen_client import *
 from sakusen_comms import *
-def debug(x): print x
+
+def debug(x): pass
 
 KIAI_SUBDIR="kiai"
+interestingthings = {} #the things we want user scripting to be able to interact with
 
 def userconfig(s):
 	configpath=fileUtils_getHome()
@@ -40,11 +41,11 @@ class connectDialog(QtGui.QDialog):
 		QtGui.QDialog.__init__(self,parent)
 		self.ui=Ui_connectDialog()
 		self.ui.setupUi(self)
+		self.connect(self,QtCore.SIGNAL("accepted()"),self.openConnection)
 	def openConnection(self):
 		global debug
 		debug("called openConnection")
 		self.emit(QtCore.SIGNAL("openConnection(QString)"),self.ui.address.text())
-def debug(x): pass
 Socket_socketsInit()
 def join(address):
 	"""Join a sakusen server at a tcp-based address"""
@@ -66,14 +67,80 @@ def join(address):
 	else:
 		debug("Got unexpected message type, dying")
                 raise Exception()
+class listener(QtCore.QObject):
+	"""A class to handle sockets and emit signals when messages are received"""
+	def __init__(self):
+		QtCore.QObject.__init__(self)
+		self.activeSocket=None
+		self.clientid=None
+		self.game=None
+	def addSocket(self,s,clientid=0):
+		debug("Adding a socket")
+		self.activeSocket=s
+		self.clientid=clientid
+		self.game=gameModel(clientid)
+		self.setSetting(('clients',str(clientid),'application','name'),'Kiai')
+	def checkPendingSockets(self):
+		b=uint8(BUFFER_LEN)
+		if(self.activeSocket):
+			s=self.activeSocket
+			l=s.receive(b,BUFFER_LEN)
+			if(l):
+				debug("Received %d bytes from socket"%l)
+				i=IArchive(b,l)
+				if(sakusen_client.cvar.world):
+					playerid=sakusen_client.cvar.world.getPlayerId()
+				else:
+					playerid=PlayerId.invalid()
+				debug("Deserializing message")
+				m=Message(i,playerid,self.game.resourceinterface)
+				debug("Getting message type")
+				t=m.getType()
+				debug("Message is of type %d"%t)
+				if(t==messageType_notifySetting):
+					d=m.getNotifySettingData()
+					if(d.getSetting()==":game:universe:name"):
+						try:
+							self.game.setUniverse(d.getValue()[0])
+						except Exception:
+							#should do some sort of error handling here
+							print "Unable to find universe \"%s\""%d.getValue()[0]
+					self.emit(QtCore.SIGNAL("settingsNotification(PyQt_PyObject)"),d)
+				elif(t==messageType_gameStart):
+					d=m.getGameStartData()
+					debug("Server wants us to start a game, our id is %s, topology %s, corners %s and %s, gravity %s, heightfield %s"%(`d.getPlayerId()`,`d.getTopology()`,`d.getTopRight()`,`d.getBottomLeft()`,`d.getGravity()`,`d.getHeightfield()`))
+					startGame(d,self.game)
+					#hope that's acted on before we do this
+					self.game.createWorld(d)
+					QtCore.QObject.connect(self,QtCore.SIGNAL("update(PyQt_PyObject)"),self.game.pushUpdates)
+				elif(t==messageType_update):
+					d=m.getUpdateData()
+					self.emit(QtCore.SIGNAL("update(PyQt_PyObject)"),d)
+				else:
+					debug("Received unexpected message of type %d"%m.getType())
+	def requestSetting(self,path):
+		s=string.join(path,':')
+		d=GetSettingMessageData(s)
+		m=Message(d)
+		d.thisown=0 #m now owns d
+		self.activeSocket.send(m)
+	def leave(self):
+		d=LeaveMessageData()
+		m=Message(d)
+		d.thisown=0 #m now owns d
+		self.activeSocket.send(m)
+	def setSetting(self,path,value):
+		s=string.join(path,':')
+		d=ChangeSettingMessageData(s,value)
+		m=Message(d)
+		d.thisown=0 #m now owns d
+		self.activeSocket.send(m)
 def openSettingsDialog(socket,clientid):
 	global a,l,s,m,mainwindow #otherwise s and m will get hilariously garbage-collected
 	l.addSocket(socket,clientid)
 	QtCore.QObject.connect(a,QtCore.SIGNAL("aboutToQuit()"),l.leave)
-	debug("going to open settings dialog") 
 	s = settingsDialog()
 	mainwindow.ui.dock.setWidget(s)
-	QtCore.QObject.connect(s,QtCore.SIGNAL("rejected()"),a,QtCore.SLOT("quit()")) #this may need to be reconsidered in the final version
 	m=settingsModel(s)
 	QtCore.QObject.connect(m,QtCore.SIGNAL("requestSetting(PyQt_PyObject)"),l.requestSetting)
 	QtCore.QObject.connect(l,QtCore.SIGNAL("settingsNotification(PyQt_PyObject)"),m.processUpdate)
@@ -127,7 +194,6 @@ mainwindow.show()
 l=listener()
 t=QtCore.QTimer()
 QtCore.QObject.connect(w,QtCore.SIGNAL("accepted()"),w.openConnection)
-QtCore.QObject.connect(w,QtCore.SIGNAL("rejected()"),a,QtCore.SLOT("quit()"))
 QtCore.QObject.connect(w,QtCore.SIGNAL("openConnection(QString)"),join)
 QtCore.QObject.connect(t,QtCore.SIGNAL("timeout()"),l.checkPendingSockets)
 t.start(10) #value in miliseconds - might want to make this less for the actual release, and more when debugging
