ad0f468b51bab3cf51ac52fbb2ab21e9b0744a75
John Bytheway <jbytheway@gmail.com>
2010-11-06 11:21:44 +0000

    Made assertion meaningful

--- sakusen/lcgiterator.h
+++ sakusen/lcgiterator.h
@@ -59,7 +59,7 @@ class LcgIterator :
   public:
     LcgIterator(uint64 const s) : state(s) { assert(state & 1); }
 
-    void reseed(uint64 const s) { assert(state & 1); state = s; }
+    void reseed(uint64 const s) { assert(s & 1); state = s; }
   private:
     friend class boost::iterator_core_access;
 
