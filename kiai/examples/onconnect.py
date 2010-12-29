8f73eda44d657baa7935ea5e0c042d3e7ec01ca7
Michael Donaghy <md401@srcf.ucam.org>
2010-12-29 18:35:43 +0000

    I want this in version control, somehow
    Signed-off-by: Michael Donaghy <md401@srcf.ucam.org>

---
+++ kiai/examples/onconnect.py
@@ -0,0 +1,4 @@
+setSetting(('clients', '0', 'neveradmin'), 'false')
+setSetting(('game', 'universe', 'name'), 'sagittarius/s')
+setSetting(('game', 'map'), 'sagittarius/s')
+setSetting(('clients', '0', 'player'), '1')
