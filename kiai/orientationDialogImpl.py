a650f708d282b246216d6960fe1202f37de060b6
Michael Donaghy <md401@srcf.ucam.org>
2010-12-29 20:59:49 +0000

    Nice compass rose
    Signed-off-by: Michael Donaghy <md401@srcf.ucam.org>

--- kiai/orientationDialogImpl.py
+++ kiai/orientationDialogImpl.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from PyQt4 import QtCore
+from PyQt4 import QtCore, Qwt5
 
 from orientationDialog import orientationDialog
 
@@ -8,3 +8,5 @@ class OrientationDialog(orientationDialog):
 	def __init__(self, *args):
 		orientationDialog.__init__(self, *args)
 	#	self.setWindowFlags(QtCore.Qt.Popup)
+		self.rose = Qwt5.QwtSimpleCompassRose()
+		self.compass.setRose(self.rose)
