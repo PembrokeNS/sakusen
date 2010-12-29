1964dbe168da5be03492569d161b522af872843d
Michael Donaghy <md401@srcf.ucam.org>
2010-12-29 19:57:48 +0000

    Manually editing designer xml: hell yeah
    Signed-off-by: Michael Donaghy <md401@srcf.ucam.org>

--- kiai/orientationDialog.py
+++ kiai/orientationDialog.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file '../kiai/orientationDialog.ui'
 #
-# Created: Wed Dec 29 19:50:52 2010
+# Created: Wed Dec 29 19:56:06 2010
 #      by: PyQt4 UI code generator 4.8.2
 #
 # WARNING! All changes made in this file will be lost!
@@ -14,26 +14,26 @@ try:
 except AttributeError:
     _fromUtf8 = lambda s: s
 
-class Ui_OrientationDialog(object):
-    def setupUi(self, OrientationDialog):
-        OrientationDialog.setObjectName(_fromUtf8("OrientationDialog"))
-        OrientationDialog.resize(114, 114)
-        self.horizontalLayout = QtGui.QHBoxLayout(OrientationDialog)
+class Ui_orientationDialog(object):
+    def setupUi(self, orientationDialog):
+        orientationDialog.setObjectName(_fromUtf8("orientationDialog"))
+        orientationDialog.resize(114, 114)
+        self.horizontalLayout = QtGui.QHBoxLayout(orientationDialog)
         self.horizontalLayout.setObjectName(_fromUtf8("horizontalLayout"))
-        self.Compass = QwtCompass(OrientationDialog)
-        self.Compass.setLineWidth(4)
-        self.Compass.setObjectName(_fromUtf8("Compass"))
-        self.horizontalLayout.addWidget(self.Compass)
+        self.compass = QwtCompass(orientationDialog)
+        self.compass.setLineWidth(4)
+        self.compass.setObjectName(_fromUtf8("compass"))
+        self.horizontalLayout.addWidget(self.compass)
 
-        self.retranslateUi(OrientationDialog)
-        QtCore.QMetaObject.connectSlotsByName(OrientationDialog)
+        self.retranslateUi(orientationDialog)
+        QtCore.QMetaObject.connectSlotsByName(orientationDialog)
 
-    def retranslateUi(self, OrientationDialog):
-        OrientationDialog.setWindowTitle(QtGui.QApplication.translate("OrientationDialog", "Form", None, QtGui.QApplication.UnicodeUTF8))
+    def retranslateUi(self, orientationDialog):
+        orientationDialog.setWindowTitle(QtGui.QApplication.translate("orientationDialog", "Form", None, QtGui.QApplication.UnicodeUTF8))
 
 from qwt_compass import QwtCompass
 
-class OrientationDialog(QtGui.QWidget, Ui_OrientationDialog):
+class orientationDialog(QtGui.QWidget, Ui_orientationDialog):
     def __init__(self, parent=None, f=QtCore.Qt.WindowFlags()):
         QtGui.QWidget.__init__(self, parent, f)
 
