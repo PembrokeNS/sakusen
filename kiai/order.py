658a0bb8f4cb60ac0d7e29a60a658b92fc74eb8a
Michael Donaghy <michaeld@mxtelecom.com>
2011-02-04 09:41:02 +0000

    More this direction, maybe
    Signed-off-by: Michael Donaghy <michaeld@mxtelecom.com>

--- kiai/order.py
+++ kiai/order.py
@@ -2,13 +2,23 @@
 from mainWindowImpl import orders
 from singleton import interestingthings
 
-availableOrders = set()
+orders = {}
+interestingthings['orders'] = orders
+availableOrders = {}
 interestingthings['availableOrders'] = availableOrders
 
-class Order:
-    def __init__(self):
-        availableOrders.add(self)
-#    def addToUi(self):
-#        self.mainwindow.ui.orders.layout().addWidget(self.button)
+class KiaiOrder:
+    def __init__(self, name):
+        availableOrders[name] = self
 
-sendSakusenOrder = Order()
+class SakusenOrder(KiaiOrder):
+	def __init__(self):
+		super.__init__(self, 'sakusenOrder')
+	def __call__(self, od):
+		od.thisown = 0
+		o = Order(od)
+	    om = OrderMessage(kwargs['unit'].getId(), o)
+		omd = OrderMessageData(om)
+	    interestingthings['socket'].sendd(omd)
+
+SakusenOrder()
