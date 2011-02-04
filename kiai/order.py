8db70106367f49ef0d162f3d684317fb0eb91c63
Michael Donaghy <michaeld@mxtelecom.com>
2011-02-04 09:13:06 +0000

    Will probably want these at some point
    Signed-off-by: Michael Donaghy <michaeld@mxtelecom.com>

--- kiai/order.py
+++ kiai/order.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
 from mainWindowImpl import orders
+from singleton import interestingthings
+
+availableOrders = set()
+interestingthings['availableOrders'] = availableOrders
 
 class Order:
     def __init__(self):
